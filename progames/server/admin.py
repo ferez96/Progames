ce0e0a2c81266fabcf59db821b8c2d428d187cfb
Duong Thai Minh <minh9a4@gmail.com>
2022-10-15 23:49:50 +0700

    add admin/users page

--- progames/server/admin.py
+++ progames/server/admin.py
@@ -7,3 +7,8 @@ bp = Blueprint("admin", __name__, url_prefix="/admin")
 @bp.route("", methods=["GET"])
 def index():
     return render_template('admin/index.html')
+
+
+@bp.route("/users", methods=["GET"])
+def users():
+    return render_template('admin/users.html')
